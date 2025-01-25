bbfcbe441a3e28ec9c404465af0194829ff0b3eb
EmuTM <blazz.umek@gmail.com>
2025-01-25 17:40:23 +0100

    SW ATTiny3226_10ch: version 1; compliant with pcb design. tested on protoboard;  to be tested in real world scenario

--- SW/LegoRCHub/ATTiny3226_10ch/ATTiny3226_10ch.ino
+++ SW/LegoRCHub/ATTiny3226_10ch/ATTiny3226_10ch.ino
@@ -1,80 +1,112 @@
-//#define DEBUG
+#define VERSION 1
 
+
+//#define DEBUG
+#ifdef DEBUG
+#define SW_SERIAL_BAUD_RATE 9600
+
+//enable this to measure PWM frequency
+//#define DEBUG_ISR_FREQ
+#endif
 
 
 #ifdef DEBUG
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(-1 /*rx not supported*/, PIN_PA1 /*any pin except UPDI*/);
 #endif
-
-
 #include "IBus.h"
-#ifdef DEBUG
-IBus iBus(&Serial, &mySerial);
-#else
-IBus iBus(&Serial/*, &mySerial*/);
-#endif
-
-
-#include "PWMPin.h"
 #include "PinDefinitions.h"
 #include <Servo_megaTinyCore.h>
+
+
+#ifdef DEBUG
+SoftwareSerial mySerial(-1 /*rx not supported*/, PIN_PC3 /*any pin except UPDI*/);
+IBus iBus(&Serial, &mySerial);
+
+#ifdef DEBUG_ISR_FREQ
+volatile uint32_t isrCounter = 0; //counter for ISR calls
+volatile uint32_t lastMillis = 0; //stores last millis for frequency calculation
+#endif
+#else
+IBus iBus(&Serial);
+#endif
 
 
 void setup()
 {
 #ifdef DEBUG
-	mySerial.begin(9600);
-	mySerial.println("DEBUG");
+	delay(3000); //this is for visual convenience when using port monitors
+	mySerial.begin(SW_SERIAL_BAUD_RATE);
+	mySerial.print("DEBUG (v");
+	mySerial.print(VERSION);
+	mySerial.println(")");
 #endif
 
 	noInterrupts();
 
 	iBus.begin();
 
-	//disable TX in the USART peripheral
+	//disable TX in the USART peripheral, so the tx pin is available (for pwm)
 	USART0.CTRLB &= ~USART_TXEN_bm;
 
-	//configure the following pins as output
+	//configure the following pins as output (for pwm)
 	PORTA.DIRSET = paPinsBitmask;
 	PORTB.DIRSET = pbPinsBitmask;
 
-	//set timer A
+	//set ISR on timer A
 	{
 		//timer_frequency_Hz = processor_clock_frequency_Hz / prescaler_value
 		//interrupt_frequency_Hz = timer_frequency_Hz / (TCA0.SINGLE.PER[timer period] + 1)
 		//pwm_frequency_Hz = timer_frequency_Hz / resolution
 
-		//for a pwm resolution of 255, this gives around 150 Hz pwm signals
+		//IMPORTANT NOTE: for a pwm resolution of 255, this gives around 150 Hz pwm signals 
+		//which seems comfortable for the processor as well as the motors
 		TCA0.SINGLE.CTRLA = TCA_SINGLE_CLKSEL_enum::TCA_SINGLE_CLKSEL_DIV8_gc | TCA_SINGLE_ENABLE_bm;
 		TCA0.SINGLE.PER = 31;
 		TCA0.SINGLE.INTCTRL = TCA_SINGLE_OVF_bm; //enable overflow interrupt
 	}
 
-	//set servos
-	{
-		servoChannels[0].attach(PIN_PC0);
-		servoChannels[1].attach(PIN_PC1);
-		servoChannels[2].attach(PIN_PC2);
-		servoChannels[3].attach(PIN_PC3);
-	}
+	//attach servos
+	ServoPin::attachAll(servoChannels, servoChannelsCount);
 
 	interrupts();
 }
 
 void loop()
 {
+#ifdef DEBUG_ISR_FREQ
+	noInterrupts(); //disable interrupts to safely access shared variables
+	uint32_t count = isrCounter;
+	uint32_t lastUpdate = lastMillis;
+	interrupts(); //re-enable interrupts
+
+	//calculate the ISR frequency once every second
+	if (millis() - lastUpdate >= 1000)
+	{
+		noInterrupts();
+
+		//update the lastMillis timestamp
+		lastMillis = millis();
+		//reset the ISR counter
+		isrCounter = 0;
+
+		interrupts();
+
+		mySerial.print("ISR Frequency: ");
+		mySerial.print(count);
+		mySerial.println(" Hz");
+	}
+#else
 	if (iBus.loop())
 	{
-
 #ifdef DEBUG
 		for (int a = 0; a < IBUS_CHANNEL_COUNT; a++)
 		{
-			mySerial.print(iBus.channels[iBusMap[a]]);
+			mySerial.print(iBus.channels[a]);
 			mySerial.print(";");
 		}
 		mySerial.println();
 #endif
+
 
 		//first 12 (6*2) channels for pwm pins
 		for (int a = 0; a < 6; a++)
@@ -134,13 +166,13 @@ void loop()
 		//next 4 channels go to servo pins
 		for (int a = 0; a < 4; a++)
 		{
-			uint8_t val = iBus.channels[iBusMap[a + 6]];
-			servoChannels[a].write(map(val, IBUS_MIN, IBUS_MAX, SERVO_MIN, SERVO_MAX));
+			uint16_t val = iBus.channels[iBusMap[a + 6]];
+			servoChannels[a].servo.write(map(val, IBUS_MIN, IBUS_MAX, SERVO_MIN, SERVO_MAX));
 
 		}
 	}
+#endif
 }
-
 
 //isr for sw pwm
 ISR(TCA0_OVF_vect)
@@ -150,16 +182,11 @@ ISR(TCA0_OVF_vect)
 
 	for (uint8_t a = 0; a < paPinsCount; a++)
 	{
-		//if (currentPwm < paPins[a].dutyCycle)
-		//{
-		//	PORTA.OUTCLR = paPins[a].bitMask; //set pin low
-		//}
-		//else
-		//{
-		//	PORTA.OUTSET = paPins[a].bitMask; //set pin hi
-		//}
-
+#ifdef PWM_INVERTED
+		if (currentPwm > paPins[a].dutyCycle)
+#else
 		if (currentPwm < paPins[a].dutyCycle)
+#endif
 		{
 			PORTA.OUTSET = paPins[a].bitMask; //set pin high
 		}
@@ -172,16 +199,11 @@ ISR(TCA0_OVF_vect)
 
 	for (uint8_t a = 0; a < pbPinsCount; a++)
 	{
-		//if (currentPwm < pbPins[a].dutyCycle)
-		//{
-		//	PORTB.OUTCLR = pbPins[a].bitMask; //set pin low
-		//}
-		//else
-		//{
-		//	PORTB.OUTSET = pbPins[a].bitMask; //set pin high
-		//}
-
+#ifdef PWM_INVERTED
+		if (currentPwm > pbPins[a].dutyCycle)
+#else
 		if (currentPwm < pbPins[a].dutyCycle)
+#endif
 		{
 			PORTB.OUTSET = pbPins[a].bitMask; //set pin high
 		}
@@ -195,6 +217,9 @@ ISR(TCA0_OVF_vect)
 	//increase or reset the counter at the end of the period
 	if (currentPwm++ >= RESOLUTION)
 	{
+#ifdef DEBUG_ISR_FREQ
+		isrCounter++;
+#endif
 		currentPwm = 0;
 	}
 
--- SW/LegoRCHub/ATTiny3226_10ch/PinDefinitions.h
+++ SW/LegoRCHub/ATTiny3226_10ch/PinDefinitions.h
@@ -1,13 +1,36 @@
 #ifndef _PINDEFINITIONS_h
 #define _PINDEFINITIONS_h
 
-#include "PWMPin.h"
+
+typedef struct PWMPin
+{
+	uint8_t bitMask;
+	uint8_t dutyCycle;
+
+	static inline uint8_t getPinsBitmask(PWMPin* pins, const uint8_t pinCount)
+	{
+		uint8_t val = 0;
+
+		for (uint8_t a = 0; a < pinCount; a++)
+		{
+			val |= pins[a].bitMask;
+		}
+
+		return val;
+	}
+} pwmPin_t;
 
 
-#define PWM_OFF 255
+//use this for sinking current pwm pins
+//#define PWM_INVERTED
+
+//IMPORTANT NOTE: this resolution is interdependant on timer A settings and should not be changed per se
 #define RESOLUTION 255
 
+//max pwm = slow decay (brake); 0 = fast decay (coast)
+#define PWM_OFF 0
 
+//IMPORTANT NOTE: these pins are defined with ISR efficiency in mind (here, only pins for port A)
 volatile pwmPin_t paPins[]
 {
 	{0x00, 0}, //PA0 UPDI //this is in the array for practical reasons, so the indexes always match
@@ -21,6 +44,7 @@ volatile pwmPin_t paPins[]
 };
 const uint8_t paPinsCount = sizeof(paPins) / sizeof(paPins[0]);
 
+//IMPORTANT NOTE: these pins are defined with ISR efficiency in mind (here, only pins for port B)
 volatile pwmPin_t pbPins[] =
 {
 	{digitalPinToBitMask(PIN_PB0), PWM_OFF},
@@ -35,74 +59,83 @@ const uint8_t pbPinsCount = sizeof(pbPins) / sizeof(pbPins[0]);
 const uint8_t paPinsBitmask = pwmPin_t::getPinsBitmask(paPins, paPinsCount);
 const uint8_t pbPinsBitmask = pwmPin_t::getPinsBitmask(pbPins, pbPinsCount);
 
-pwmPin_t* pwmChannels[12]
+//IMPORTANT NOTE: maps pwm channels to pins that drive H bridges
+//THIS IS PCB DESIGN DEPENDANT!
+pwmPin_t* pwmChannels[6 * 2]
 {
-	&paPins[4], //ch0_up
-	&paPins[3], //ch0_down
+	&paPins[5], //ch0_up (ch1_2)
+	&paPins[4], //ch0_down (ch1_3)
 
-	&paPins[5], //ch1_up
-	&paPins[2], //ch1_down
+	&pbPins[5], //ch1_up (ch2_2)
+	&pbPins[4], //ch1_down (ch2_3)
 
-	&paPins[6], //ch2_up
-	&paPins[1], //ch2_down
+	&paPins[7], //ch2_up (ch3_2)
+	&paPins[6], //ch2_down (ch3_3)
 
-	&pbPins[0], //ch3_up
-	&pbPins[1], //ch3_down
+	&paPins[3], //ch3_up (ch4_2)
+	&paPins[2], //ch3_down (ch4_3)
 
-	&pbPins[2], //ch4_up
-	&paPins[7], //ch4_down
+	&pbPins[1], //ch4_up (ch5_2)
+	&pbPins[2], //ch4_down (ch5_3)
 
-	&pbPins[5], //ch5_up
-	&pbPins[4]  //ch5_down
+	&paPins[1], //ch5_up (ch6_2)
+	&pbPins[0]  //ch5_down (ch6_3)
 };
 
 
 #include <Servo_megaTinyCore.h>
+typedef struct ServoPin
+{
+	uint8_t pin;
+	Servo servo;
+
+	static inline void attachAll(ServoPin* pins, const uint8_t pinCount)
+	{
+		for (int a = 0; a < pinCount; a++)
+		{
+			pins[a].servo.attach(pins[a].pin);
+		}
+	}
+
+} servoPin_t;
 
 #define SERVO_MIN 0
 #define SERVO_MAX 180
 
-const Servo servoChannels[]
+//IMPORTANT NOTE: maps servo channels to pins that drive servos
+//THIS IS PCB DESIGN DEPENDANT!
+const servoPin_t servoChannels[4]
 {
-	Servo(),
-	Servo(),
-	Servo(),
-	Servo()
+	{PIN_PC3, Servo()}, //ch7
+	{PIN_PC0, Servo()}, //ch8
+	{PIN_PC2, Servo()}, //ch9
+	{PIN_PC1, Servo()} //ch10
 };
+const uint8_t servoChannelsCount = sizeof(servoChannels) / sizeof(servoChannels[0]);
 
+
+#include "IBus.h"
+
+//USE THIS TO MAP IBUS SIGNALS TO OUTPUT CHANNELS
+//number in here define the pwm/servo channel (channels 0-5 are pwm; channels 6-9 are servos) with respect to iBus channel
 uint8_t iBusMap[IBUS_CHANNEL_COUNT]
 {
+	0, 
 	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1,
-	1
+	2,
+	3,
+	4,
+	5,
+	6,
+	7,
+	8,
+	9,
 
-	//0,
-	//1,
-	//2,
-	//3,
-	//4,
-	//5,
-	//6,
-	//7,
-	//8,
-	//9,
-	//10,
-	//11,
-	//12,
-	//13
+
+	10, //do not use - no pin available
+	11, //do not use - no pin available
+	12, //do not use - no pin available
+	13  //do not use - no pin available
 };
-
-
 
 #endif
